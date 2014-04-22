a3d7d3c38859204bcac59bb81e6309d7bab7acb6
usuario <usuario@172.16.6.46>
2014-04-22 11:11:32 -0500

    Se agregó el codigo para la lectura de archivos de la reconcha de su madre

--- Aeropuerto/src/aeropuerto/Nodo.java
+++ Aeropuerto/src/aeropuerto/Nodo.java
@@ -3,34 +3,59 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package aeropuerto;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
  * @author sofia
  */
 public class Nodo {
-     int Dato;
+
+    int Dato;
     Nodo sig;
     Nodo ant;
-    public Nodo (int entrada)
-    {
+
+    public Nodo(int entrada) {
         Dato = entrada;
         sig = ant = null;
-        
+
         //this.Dato=a;
         //this.sig=null;
         //this.ant=null;
     }
-    public int getDato(){
+
+    public int getDato() {
         return Dato;
     }
-    public Nodo getEnlace(){
+
+    public Nodo getEnlace() {
         return sig;
     }
-    
-    public void setEnlace(Nodo sig){
+
+    public void setEnlace(Nodo sig) {
         this.sig = sig;
+    }
+
+    public class Lectura_Archivos {
+
+        public void main(String args[]) {
+            String fichero = args[0];
+            try {
+                try (FileReader fr = new FileReader(fichero)) {
+                    BufferedReader br = new BufferedReader(fr);
+
+                    String Linea;
+                    while ((Linea = br.readLine()) != null) {
+                        System.out.println(Linea);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Error en lectura del archivo");
+            }
+        }
     }
 }
